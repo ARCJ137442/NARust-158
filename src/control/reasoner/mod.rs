7ee25c3c3ba4874359c5be0923184982f8038857
ARCJ137442 <61109168+ARCJ137442@users.noreply.github.com>
2024-06-26 12:07:02 +0800

    feat: :construction: 初步完成「推理器」自身的模块架构

--- src/control/reasoner/mod.rs
+++ src/control/reasoner/mod.rs
@@ -5,10 +5,12 @@
 //! * ♻️【2024-06-26 11:48:57】目前开始着手按OpenNARS改版重写
 
 nar_dev_utils::mods! {
-    // // 定义
-    // pub use definition;
-    // // 功能：解析Narsese任务
-    // pub use parse_task;
-    // // 功能：输出报告
-    // pub use report;
+    // 定义
+    pub use definition;
+
+    // 功能：解析Narsese任务
+    pub use parse_task;
+
+    // 功能：输出报告
+    pub use report;
 }
